0802d07bfac5aedc1dbef9f57c1a0c83a4bcfbaa
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-11-22 10:33:34 +0100

    add copy right

--- src/syn_retro/__init__.py
+++ src/syn_retro/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.0.0"
+
+#
+#  Copyright (c) 2023 Synple Chem AG
+#  All rights reserved.
+#
+#  This file is part of the ChEMBL_StructurePipeline project.
+#  The contents are covered by the terms of the MIT license
+#  which is included in the file LICENSE, found at the root
+#  of the source tree.
